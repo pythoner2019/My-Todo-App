971c10d314e1c2d3b9ac48db41a83ae5b456ff3e
Your Name <you@example.com>
2018-10-03 16:37:39 +0200

    add GET /todo:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 // Library import
 const express = require('express');
 const bodyParser = require('body-parser');  // send json to server : server take json and do some, parse the body take string and change to js object.
+const {ObjectID} = require('mongodb');
 
 // Local import
 const {mongoose} = require('./db/mongoose');
@@ -34,6 +35,27 @@ app.get('/todos', (req, res) => {
 			res.status(400).send(e)
 		})
 })
+
+app.get('/todos/:id', (req, res) => {
+	const id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send()
+	}
+
+	Todo.findById(id)
+		.then((todo) => {
+			if (!todo) {
+				return res.status(404).send()
+			}
+
+			res.send({todo});
+		})
+		.catch((err) => {
+			res.status(400).send();
+		})
+})
+
 
 
 app.listen(3000, () => {
