9444ee42fc7a0821b41d46950925676ddbf06ec7
Your Name <you@example.com>
2018-10-03 14:34:45 +0200

    get and post postman

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
+// Library import
 const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+const bodyParser = require('body-parser');  // send json to server : server take json and do some, parse the body take string and change to js object.
 
+// Local import
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./model/todo');
 const {User} = require('./model/user');
 
-
+const app = express();
 app.use(bodyParser.json())
 
-
+// crud operation (create resource)
 app.post('/todos', (req, res) => {
 	const todo = new Todo({
 		text: req.body.text
@@ -24,6 +25,16 @@ app.post('/todos', (req, res) => {
 		})
 })
 
+app.get('/todos', (req, res) => {
+	Todo.find()
+		.then((todos) => {
+			res.send({todos});
+		})
+		.catch((e) => {
+			res.status(400).send(e)
+		})
+})
+
 
 app.listen(3000, () => {
 	console.log('started on port 3000');
@@ -31,6 +42,7 @@ app.listen(3000, () => {
 
 
 
+module.exports = {app};
 
 
 
@@ -42,3 +54,6 @@ app.listen(3000, () => {
 
 
 
+
+
+// Path on Terminal >>>>    C:\Program Files\MongoDB\Server\4.0\bin>mongod.exe --dbpath /Users/Muhammed/mongo-data
