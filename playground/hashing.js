0068f20ecac00cb37eec76f7684b1dd49e53e288
Your Name <you@example.com>
2018-10-12 11:59:45 +0200

    Hash user Password Before Saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,21 +1,55 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+
+const password = '123abc';
+
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	})
+// })
+
+
+const hashedPassword = '$2a$10$CZHhxK9jwFFR4/HZVM.dzuc5cYL4FJ1PxC/5nh6WXqa8i1IHE06Xa';
+
+
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // just do 2 functions : 1 to create token and other for verify it
 // jwt.sign()      jwt.verify()
 
-const data = {
-	id: 10
-};
+// const data = {
+// 	id: 10
+// };
 
 
-// create Token: with secret 123abc:
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// // create Token: with secret 123abc:
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
 
-// Now Verify:  with same secret (must) 123abc:
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded: ', decoded)
+// // Now Verify:  with same secret (must) 123abc:
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded: ', decoded)
 
 /*--------------------------------------------------------------------*/
 
